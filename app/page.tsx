8eb5c8ca1f503b0df9ea2047bfc97ef575e125ec
Kayque Goldner <kayquegoldner@gmail.com>
2025-03-28 08:33:01 -0300

    landing page hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import { Button } from "@/components/ui/button";
+import { BgGradient } from "@/components/common/bg-gradient";
+import { HeroSection } from "@/components/home/hero-section";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="outline" size="lg">
-        Click me
-      </Button>
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
+      {/* <DemoSection /> */}
+      {/* <HowItWorksSection /> */}
+      {/* <PricingSection /> */}
+      {/* <CTA /> */}
     </div>
   );
 }
