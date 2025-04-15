b9cbf88c5169631b831c173811188a06f004d188
Kayque Goldner <kayquegoldner@gmail.com>
2025-04-14 22:08:29 -0300

    add clerk authentication

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignIn } from "@clerk/nextjs";
+
+import { BgGradient } from "@/components/common/bg-gradient";
+
+export default function Page() {
+  return (
+    <section className="flex items-center justify-center lg:min-h-[40vh]">
+      <div className="mx-auto max-w-5xl px-4 py-12 sm:px-6 lg:px-8 lg:py-24 lg:pt-12">
+        <BgGradient className="from-rose-500 via-rose-300 to-orange-200 opacity-100" />
+        <SignIn />
+      </div>
+    </section>
+  );
+}
--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,11 +1,9 @@
 import { FileTextIcon } from "lucide-react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
-import { Button } from "@/components/ui/button";
 import { NavLink } from "@/components/common/nav-link";
 
 export const Header = () => {
-  const isLoggedIn = false;
-
   return (
     <nav className="container mx-auto flex items-center justify-between px-2 py-4 lg:px-8">
       <div className="flex lg:flex-1">
@@ -19,21 +17,22 @@ export const Header = () => {
 
       <div className="flex gap-4 lg:items-center lg:justify-center lg:gap-12">
         <NavLink href="/#pricing">Pricing</NavLink>
-        {isLoggedIn && <NavLink href="/dashboard">Your Summaries</NavLink>}
+        <SignedIn>
+          <NavLink href="/dashboard">Your Summaries</NavLink>
+        </SignedIn>
       </div>
 
       <div className="flex lg:flex-1 lg:justify-end">
-        {isLoggedIn ? (
+        <SignedIn>
           <div className="flex items-center gap-2">
             <NavLink href="/upload">Upload a PDF</NavLink>
             <div>Pro</div>
-            <Button>User</Button>
+            <UserButton />
           </div>
-        ) : (
-          <div>
-            <NavLink href="/sign-in">Sign In</NavLink>
-          </div>
-        )}
+        </SignedIn>
+        <SignedOut>
+          <NavLink href="/sign-in">Sign In</NavLink>
+        </SignedOut>
       </div>
     </nav>
   );
